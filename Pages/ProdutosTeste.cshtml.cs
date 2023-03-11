77cea5281903bd217f6388729cd6a87dc7027e2c
nunomivieira <nuno11vieira@hotmail.com>
2023-03-11 16:31:16 +0000

    Pus mais produtos

---
+++ Pages/ProdutosTeste.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projeto.Pages
+{
+    public class ProdutosTesteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
